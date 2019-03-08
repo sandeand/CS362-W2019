294c53e5e8e36cbe9ac76e6ef85ee235b73a11c6
sandeand <39066304+sandeand@users.noreply.github.com>
2019-03-08 12:42:13 -0800

    Add files via upload

---
+++ projects/laxsont/dominion/cardtest2.c
@@ -0,0 +1,63 @@
+#include "dominion.h"
+#include <stdio.h>
+
+int asserttrue(int a, int b, int line){
+  if(a == b){
+    //printf("Test Successful for a=%d, b=%d.\n", a, b); 
+    return 0;
+  }
+  else{
+    printf("Test Unsuccessful for out=%d, expected=%d.\n\tFound on line %d.\n", a, b, line);
+    return 1;
+  }
+}
+
+// TESTING SUITE FOR CARD __
+// adventurer
+
+int main(){
+  // Setup the game
+  int outcome = 0, r;
+  struct gameState game;
+  int cards[10] = {adventurer, council_room, feast, gardens, mine,
+	       remodel, smithy, village, baron, great_hall};
+
+  r = initializeGame(2, cards, 1, &game);
+
+  // Execute tests
+  //outcome += asserttrue(int returning function, correct value);
+  //outcome += asserttrue(numHandCards(&game), 5);
+  game.whoseTurn = 0;
+  game.handCount[0] = 1;
+  game.handCount[1] = 0;
+  game.hand[0][0] = adventurer; 
+  game.numActions = 1;
+
+  playCard(0, -1, -1, -1, &game);
+
+  outcome += asserttrue(game.handCount[0], 2, __LINE__);
+  outcome += asserttrue(game.handCount[1], 0, __LINE__);
+   
+  game.handCount[0] = 2;
+  game.handCount[1] = 0;
+  game.hand[0][0] = adventurer; 
+  game.numActions = 1;
+
+  playCard(0, -1, -1, -1, &game);
+
+  outcome += asserttrue(game.handCount[0], 3, __LINE__);
+  outcome += asserttrue(game.handCount[1], 0, __LINE__);
+
+  if(outcome == 0){
+    printf("Testing suite successful!\n");
+    return 0;
+  }
+  if(outcome != 0){
+    printf("Testing suite unsuccessful! %d errors found!\n", outcome); 
+    return 0;
+  }
+}
+
+
+
+
